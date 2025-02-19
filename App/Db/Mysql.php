912f93de3b79d33be94c7bada98bb34c1304cbb5
Nackito <nackfrank@gmail.com>
2025-02-19 21:16:48 +0100

    modif mysql

--- App/Db/Mysql.php
+++ App/Db/Mysql.php
@@ -3,31 +3,53 @@
 namespace App\Db;
 
 use PDO;
-use PDOException;
 
 class Mysql
 {
-  private $pdo;
+  private $db_name;
+  private $db_user;
+  private $db_password;
+  private $db_host;
+  private $db_port;
+  private $pdo = null;
+  private static $_instance = null;
 
-  public function __construct()
+  private function __construct()
   {
-    $config = require __DIR__ . '/../../db_config.php';
-    $dbConfig = $config['db'];
+    $conf = require_once _ROOTPATH_ . '/db_config.php';
 
-    try {
-      $this->pdo = new PDO(
-        "mysql:host={$dbConfig['db_host']};dbname={$dbConfig['db_name']};port={$dbConfig['db_port']}",
-        $dbConfig['db_user'],
-        $dbConfig['db_password']
-      );
-      $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-      throw new PDOException("Erreur de connexion à la base de données : " . $e->getMessage());
+    if (isset($conf['db']['db_name'])) {
+      $this->db_name = $conf['db']['db_name'];
+    }
+    if (isset($conf['db']['db_user'])) {
+      $this->db_user = $conf['db']['db_user'];
+    }
+    if (isset($conf['db']['db_password'])) {
+      $this->db_password = $conf['db']['db_password'];
+    }
+    if (isset($conf['db']['db_host'])) {
+      $this->db_host = $conf['db']['db_host'];
+    }
+    if (isset($conf['db']['db_port'])) {
+      $this->db_port = $conf['db']['db_port'];
     }
   }
 
-  public function getPDO()
+  // Son rôle est soit de nous donner une instance ou de créer une instance s'il n'y en a pas
+  public static function getInstance(): self
   {
+    if (is_null(self::$_instance)) {
+      self::$_instance = new Mysql();
+    }
+    return self::$_instance;
+  }
+
+  public function getPDO(): PDO
+  {
+    if (is_null($this->pdo)) {
+      $dsn = 'mysql:host=' . $this->db_host . ';port=' . $this->db_port . ';dbname=' . $this->db_name;
+      $this->pdo = new PDO($dsn, $this->db_user, $this->db_password);
+    }
     return $this->pdo;
   }
 }
